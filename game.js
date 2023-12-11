e4aaad9056829552123347a8338fcf686c77b8c0
Savanna <149965311+vannatatro@users.noreply.github.com>
2023-12-11 17:25:50 -0500

    Create playRound function and outcomes variables

--- game.js
+++ game.js
@@ -11,4 +11,58 @@ function getComputerChoice() {
     let comChoice = options[randomNum]
     return(comChoice)
 }
-console.log(getComputerChoice())
+
+// User enters "Rock", "Paper", or "Scissors"
+function getPlayerChoice () {
+    let playerChoice = prompt('Enter rock, paper, or scissors')
+    let playerChoiceLower = playerChoice.toLowerCase()
+    return(playerChoiceLower)
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+console.log(computerSelection)
+console.log(playerSelection)
+
+// Outcomes
+ifTie = (computerSelection === playerSelection)
+ifComRock = (computerSelection === "rock");
+IfComPaper = (computerSelection === "paper");
+ifComScissors = (computerSelection === "scissors");
+ifPlayScissors = (playerSelection === "scissors");
+ifPlayRock = (playerSelection === "rock");
+ifPlayPaper = (playerSelection === "paper");
+
+// Play round
+function playRound(playerSelection, computerSelection) {
+    // If tie:
+    if (ifTie == true) {
+        tie = "Tie! Please try again";
+        return(tie);
+
+    // If computer chooses rock:
+    } else if (ifComRock == true) {
+            if (ifPlayScissors == true) {
+                return("You lose! Rock breaks scissors!")
+            } else if (ifPlayPaper == true) {
+                return("You win! Paper covers rock!")
+            }
+
+    // If computer chooses paper:
+    } else if (IfComPaper == true) {
+            if (ifPlayScissors == true) {
+                return("You win! Scissors cut paper!")
+            } else if (ifPlayRock) {
+                    return("You lose! Paper covers rock!")
+            }
+
+    // If computer chooses scissors:
+    } else if (ifComScissors == true) {
+            if (ifPlayPaper) {
+                return("You lose! Scissors cut paper!")
+            } else if (ifPlayRock) {
+                return("You win! Rock breaks scissors!")
+            }
+    }
+}
+console.log(playRound())
